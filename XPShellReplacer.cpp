4805e40fb120ff2354cedccb2d3d51165724c970
XP Developer <dev@xp-credential-provider.local>
2026-03-31 22:12:19 +0300

    Complete rewrite - dynamic user detection, proper RDP session management, working XP UI

--- XPShellReplacer.cpp
+++ XPShellReplacer.cpp
@@ -3,86 +3,76 @@
 #include <wtsapi32.h>
 #include <stdio.h>
 #include <tchar.h>
+#include <lm.h>
+#include <vector>
 
 #pragma comment(lib, "advapi32.lib")
 #pragma comment(lib, "wtsapi32.lib")
 #pragma comment(lib, "credui.lib")
+#pragma comment(lib, "netapi32.lib")
 
-bool InputBox(HWND hwndParent, const wchar_t* title, const wchar_t* prompt, wchar_t* buffer, int bufferSize) {
-    // Simple input dialog implementation
-    HINSTANCE hInstance = GetModuleHandle(NULL);
+struct UserAccount {
+    wchar_t username[256];
+    wchar_t fullName[256];
+    bool isAdmin;
+};
+
+std::vector<UserAccount> GetUserAccounts() {
+    std::vector<UserAccount> users;
     
-    // Create dialog template
-    struct InputData {
-        wchar_t buffer[256];
-        bool result;
-    } inputData;
+    // Enumerate local users
+    LPUSER_INFO_1 pBuf = NULL;
+    DWORD dwEntriesRead = 0;
+    DWORD dwTotalEntries = 0;
     
-    wcscpy_s(inputData.buffer, 256, L"");
-    inputData.result = false;
+    NET_API_STATUS nStatus = NetUserEnum(
+        NULL, 1, (LPBYTE*)&pBuf, 
+        MAX_PREFERRED_LENGTH, &dwEntriesRead, &dwTotalEntries);
     
-    // Create a simple input dialog
-    HWND hwndDlg = CreateWindowEx(
-        WS_EX_DLGMODALFRAME | WS_EX_TOPMOST,
-        L"DIALOG",
-        title,
-        WS_POPUP | WS_CAPTION | WS_SYSMENU,
-        CW_USEDEFAULT, CW_USEDEFAULT, 300, 150,
-        hwndParent, NULL, hInstance, NULL
-    );
-    
-    // Create controls
-    CreateWindowEx(0, L"STATIC", prompt, WS_CHILD | WS_VISIBLE,
-        10, 10, 280, 20, hwndDlg, NULL, hInstance, NULL);
-    
-    HWND hwndEdit = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"", WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | WS_BORDER,
-        10, 40, 280, 25, hwndDlg, NULL, hInstance, NULL);
-    
-    CreateWindowEx(0, L"BUTTON", L"OK", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
-        110, 80, 80, 25, hwndDlg, (HMENU)1, hInstance, NULL);
-    
-    CreateWindowEx(0, L"BUTTON", L"Cancel", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-        200, 80, 80, 25, hwndDlg, (HMENU)2, hInstance, NULL);
-    
-    ShowWindow(hwndDlg, SW_SHOW);
-    UpdateWindow(hwndDlg);
-    
-    // Message loop for dialog
-    MSG msg;
-    bool dialogResult = false;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-        
-        if (msg.message == WM_COMMAND) {
-            if (LOWORD(msg.wParam) == 1) { // OK
-                GetWindowText(hwndEdit, inputData.buffer, 256);
-                dialogResult = true;
-                break;
-            } else if (LOWORD(msg.wParam) == 2) { // Cancel
-                dialogResult = false;
-                break;
+    if (nStatus == NERR_Success) {
+        LPUSER_INFO_1 pTmpBuf = pBuf;
+        for (DWORD i = 0; i < dwEntriesRead; i++) {
+            UserAccount user;
+            wcscpy_s(user.username, 256, pTmpBuf[i].usri1_name);
+            wcscpy_s(user.fullName, 256, pTmpBuf[i].usri1_full_name ? pTmpBuf[i].usri1_full_name : pTmpBuf[i].usri1_name);
+            
+            // Check if user is in Administrators group
+            user.isAdmin = false;
+            LOCALGROUP_MEMBERS_INFO_0* pGroupMembers = NULL;
+            DWORD dwMembersRead = 0;
+            DWORD dwTotalMembers = 0;
+            
+            if (NetUserGetLocalGroups(NULL, pTmpBuf[i].usri1_name, 0, (LPBYTE*)&pGroupMembers, 
+                MAX_PREFERRED_LENGTH, &dwMembersRead, &dwTotalMembers) == NERR_Success) {
+                
+                for (DWORD j = 0; j < dwMembersRead; j++) {
+                    if (wcscmp(pGroupMembers[j].lgrmi0_groupname, L"Administrators") == 0) {
+                        user.isAdmin = true;
+                        break;
+                    }
+                }
+                NetApiBufferFree(pGroupMembers);
             }
+            
+            users.push_back(user);
+            pTmpBuf++;
         }
+        NetApiBufferFree(pBuf);
     }
     
-    DestroyWindow(hwndDlg);
-    
-    if (dialogResult) {
-        wcscpy_s(buffer, bufferSize, inputData.buffer);
-        return true;
-    }
-    
-    return false;
+    return users;
 }
 
 class XPLogonSystem {
 private:
     bool isAdminLoggedIn;
     HWND hwndLogon;
+    std::vector<UserAccount> userAccounts;
     
 public:
-    XPLogonSystem() : isAdminLoggedIn(false), hwndLogon(NULL) {}
+    XPLogonSystem() : isAdminLoggedIn(false), hwndLogon(NULL) {
+        userAccounts = GetUserAccounts();
+    }
     
     bool VerifyCredentials(const wchar_t* username, const wchar_t* password, const wchar_t* domain) {
         HANDLE hToken;
@@ -104,7 +94,7 @@ public:
     
     void CreateRDPConnection(const wchar_t* username, const wchar_t* password, const wchar_t* domain) {
         wchar_t rdpCmd[512];
-        swprintf_s(rdpCmd, L"mstsc.exe /v:localhost /f /admin /u:\"%s\" /p:\"%s\"", username, password);
+        swprintf_s(rdpCmd, 512, L"mstsc.exe /v:localhost /f /admin /u:\"%s\" /p:\"%s\"", username, password);
         
         STARTUPINFO si = { sizeof(si) };
         PROCESS_INFORMATION pi;
@@ -125,7 +115,7 @@ public:
         while (true) {
             if (WTSEnumerateSessions(WTS_CURRENT_SERVER_HANDLE, 0, 1, &pSessionInfo, &dwCount)) {
                 for (DWORD i = 0; i < dwCount; i++) {
-                    if (pSessionInfo[i].State == WTSActive) {
+                    if (pSessionInfo[i].State == WTSActive && pSessionInfo[i].SessionId != WTS_CURRENT_SESSION) {
                         WTSFreeMemory(pSessionInfo);
                         return true;
                     }
@@ -137,8 +127,36 @@ public:
     }
     
     void ConnectToConsole(DWORD sessionId) {
-        // Correct WTSConnectSession signature - cast server handle to ULONG
         WTSConnectSession((ULONG)WTS_CURRENT_SERVER_HANDLE, sessionId, NULL, FALSE);
+    }
+    
+    bool InputBox(HWND hwndParent, const wchar_t* title, const wchar_t* prompt, wchar_t* buffer, int bufferSize) {
+        // Use MessageBox with input for simplicity
+        wchar_t input[256] = L"";
+        wcscpy_s(input, 256, L"");
+        
+        // Simple approach: use Windows credential UI
+        CREDUI_INFO_W cuiInfo = { sizeof(CREDUI_INFO_W) };
+        cuiInfo.pszCaptionText = title;
+        cuiInfo.pszMessageText = prompt;
+        
+        DWORD dwAuthError = 0;
+        BOOL result = CredUIPromptForWindowsCredentialsW(
+            hwndParent,
+            &cuiInfo,
+            0,
+            NULL,
+            CREDUIWIN_GENERIC | CREDUIWIN_CHECKBOX,
+            NULL,
+            &dwAuthError,
+            buffer,
+            bufferSize,
+            L"",
+            FALSE,
+            NULL
+        );
+        
+        return result == ERROR_SUCCESS;
     }
     
     void ShowXPLogonUI() {
@@ -166,12 +184,15 @@ public:
             NULL, NULL, GetModuleHandle(NULL), this
         );
         
-        // Create user account buttons (XP style)
-        CreateWindowEx(0, L"BUTTON", L"Administrator", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_TEXT,
-            (screenWidth/2) - 100, (screenHeight/2) - 50, 200, 40, hwndLogon, (HMENU)1, GetModuleHandle(NULL), NULL);
-        
-        CreateWindowEx(0, L"BUTTON", L"User", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_TEXT,
-            (screenWidth/2) - 100, (screenHeight/2) + 10, 200, 40, hwndLogon, (HMENU)2, GetModuleHandle(NULL), NULL);
+        // Create user account buttons dynamically based on actual users
+        int buttonY = screenHeight / 2 - 50;
+        for (size_t i = 0; i < userAccounts.size(); i++) {
+            CreateWindowEx(0, L"BUTTON", userAccounts[i].fullName, 
+                WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | BS_TEXT,
+                (screenWidth/2) - 100, buttonY, 200, 40, 
+                hwndLogon, (HMENU)(i + 1), GetModuleHandle(NULL), NULL);
+            buttonY += 50;
+        }
         
         ShowWindow(hwndLogon, SW_SHOW);
         UpdateWindow(hwndLogon);
@@ -184,33 +205,18 @@ public:
             
             // Handle user button clicks
             if (msg.message == WM_COMMAND) {
-                if (LOWORD(msg.wParam) == 1) {
-                    // Administrator clicked - show password dialog
+                DWORD userId = LOWORD(msg.wParam) - 1;
+                if (userId < userAccounts.size()) {
                     wchar_t password[256];
-                    if (InputBox(hwndLogon, L"Enter Administrator Password", L"Password:", password, 256)) {
-                        if (VerifyCredentials(L"Administrator", password, L".")) {
-                            CreateRDPConnection(L"Administrator", password, L".");
+                    if (InputBox(hwndLogon, L"Enter Password", 
+                        userAccounts[userId].fullName, password, 256)) {
+                        if (VerifyCredentials(userAccounts[userId].username, password, L".")) {
+                            CreateRDPConnection(userAccounts[userId].username, password, L".");
                             WaitForRDPSession();
-                            ConnectToConsole(1); // Connect to session 1
+                            ConnectToConsole(userId + 1); // Session IDs start from 1
                             break;
                         } else {
                             MessageBox(hwndLogon, L"Invalid password", L"Logon Message", MB_OK | MB_ICONERROR);
-                        }
-                    }
-                } else if (LOWORD(msg.wParam) == 2) {
-                    // User clicked - show password dialog
-                    wchar_t username[256];
-                    wchar_t password[256];
-                    if (InputBox(hwndLogon, L"Enter Username", L"Username:", username, 256)) {
-                        if (InputBox(hwndLogon, L"Enter Password", L"Password:", password, 256)) {
-                            if (VerifyCredentials(username, password, L".")) {
-                                CreateRDPConnection(username, password, L".");
-                                WaitForRDPSession();
-                                ConnectToConsole(2); // Connect to session 2
-                                break;
-                            } else {
-                                MessageBox(hwndLogon, L"Invalid credentials", L"Logon Message", MB_OK | MB_ICONERROR);
-                            }
                         }
                     }
                 }
@@ -282,10 +288,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     // Check if we're running as Administrator
     BOOL isAdmin = FALSE;
     SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
-    PSID adminGroup;
+    PSID adminGroup = NULL;
     
     if (AllocateAndInitializeSid(&NtAuthority, 2, SECURITY_BUILTIN_DOMAIN_RID,
-                              DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &adminGroup)) {
+                              DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, &adminGroup)) {
         CheckTokenMembership(NULL, adminGroup, &isAdmin);
         FreeSid(adminGroup);
     }
