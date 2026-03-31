37b6c2214f066bca1c0af91ade4b46914cb1a3be
XP Developer <dev@xp-credential-provider.local>
2026-03-31 22:01:10 +0300

    Add actual functionality to XP Shell Replacer - username/password fields and logon button

--- XPShellReplacer.cpp
+++ XPShellReplacer.cpp
@@ -93,6 +93,16 @@ public:
             NULL, NULL, GetModuleHandle(NULL), this
         );
         
+        // Create input fields
+        CreateWindowEx(0, L"EDIT", L"Username", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+            400, 200, 200, 25, hwndLogon, (HMENU)1, GetModuleHandle(NULL), NULL);
+        
+        CreateWindowEx(0, L"EDIT", L"Password", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL | ES_PASSWORD,
+            400, 240, 200, 25, hwndLogon, (HMENU)2, GetModuleHandle(NULL), NULL);
+        
+        CreateWindowEx(0, L"BUTTON", L"Log On", WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+            400, 280, 100, 30, hwndLogon, (HMENU)3, GetModuleHandle(NULL), NULL);
+        
         ShowWindow(hwndLogon, SW_SHOW);
         UpdateWindow(hwndLogon);
         
@@ -101,6 +111,24 @@ public:
         while (GetMessage(&msg, NULL, 0, 0)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+            
+            // Handle logon button click
+            if (msg.message == WM_COMMAND && LOWORD(msg.wParam) == 3) {
+                wchar_t username[256];
+                wchar_t password[256];
+                
+                GetWindowText(GetDlgItem(hwndLogon, 1), username, 256);
+                GetWindowText(GetDlgItem(hwndLogon, 2), password, 256);
+                
+                if (VerifyCredentials(username, password, L".")) {
+                    CreateRDPConnection(username, password, L".");
+                    WaitForRDPSession();
+                    // TODO: Connect to actual session and logout admin
+                    break;
+                } else {
+                    MessageBox(hwndLogon, L"Invalid credentials", L"Error", MB_OK | MB_ICONERROR);
+                }
+            }
         }
     }
     
@@ -108,6 +136,10 @@ public:
         XPLogonSystem* pThis = (XPLogonSystem*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
         
         switch (msg) {
+            case WM_CREATE:
+                SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)lParam);
+                break;
+                
             case WM_PAINT: {
                 PAINTSTRUCT ps;
                 HDC hdc = BeginPaint(hwnd, &ps);
@@ -120,7 +152,11 @@ public:
                 // Draw welcome text
                 SetTextColor(hdc, RGB(255, 255, 255));
                 SetBkMode(hdc, TRANSPARENT);
-                DrawText(hdc, L"Welcome to Windows XP", -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+                DrawText(hdc, L"Welcome to Windows XP", -1, &rect, DT_CENTER | DT_TOP | DT_SINGLELINE);
+                
+                // Draw input field labels
+                TextOut(hdc, 320, 180, L"Username:", 9);
+                TextOut(hdc, 320, 220, L"Password:", 9);
                 
                 EndPaint(hwnd, &ps);
                 return 0;
